3c9c0cb09dd692d8c7bf85b1376128fd5cdab0d5
markup54 <marzenakupka@tlen.pl>
2022-05-12 09:06:39 +0200

    dyrektywy

---
+++ todo/dist/todo.js
@@ -0,0 +1,28 @@
+"use strict";
+class Todo {
+    constructor(nazwa) {
+        this._nazwa = nazwa;
+        this._wykonane = false;
+        Todo.licznik++;
+    }
+    get nazwa() {
+        return this._nazwa;
+    }
+    get wykonane() {
+        return this._wykonane;
+    }
+    set nazwa(value) {
+        this._nazwa = value;
+    }
+    set wykonane(value) {
+        this._wykonane = value;
+    }
+}
+Todo.licznik = 0;
+const rzecz1 = new Todo("Podlać kwiatki");
+rzecz1.wykonane = true;
+console.log(rzecz1.nazwa);
+console.log(Todo.licznik);
+const rzecz2 = new Todo("Karmić psa");
+console.log(rzecz2.nazwa);
+console.log(Todo.licznik);
